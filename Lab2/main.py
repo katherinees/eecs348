aa8f66ecb7627b4ec4ee241835bd7d15dea90000
katherinees <katherinesteiner2019@u.northwestern.edu>
2018-02-04 15:26:49 -0600

    new main.py with updated tests thx willie for nothing

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,7 +1,7 @@
 import copy
 import random
 import konane as kb
-import student_code as sc
+import student_code as student
 
 # sample boards used for evaluation of student implementations
 
@@ -24,47 +24,56 @@ boardC = [['x','o','x','o','x','o'],
 
 # 'gold' sequences of moves that students' implementations must match
 
-ans1 = [((2, 1), (0, 1)), ((2, 3), (2, 1))]
+ans1 = [((0, 3), (0, 1)), ((2, 3), (0, 3)), ((2, 1), (2, 3))]
 
 ans2 = [((3, 5), (3, 3)), ((4, 2), (2, 2)),
-        ((1, 3), (1, 1)), ((4, 4), (2, 4)),
-        ((5, 5), (3, 5)), ((5, 3), (5, 5)),
-        ((5, 1), (5, 3)), ((4, 0), (4, 2)),
-        ((3, 1), (1, 1)), ((4, 2), (2, 2)),
-        ((2, 2), (2, 0))]
+        ((1, 3), (1, 1)), ((3, 3), (1, 3)),
+        ((4, 4), (2, 4)), ((5, 5), (3, 5)),
+        ((4, 0), (4, 2)), ((2, 0), (4, 0)),
+        ((5, 3), (5, 5)), ((4, 2), (4, 4)),
+        ((0, 4), (0, 2)), ((4, 0), (4, 2)),
+        ((5, 1), (5, 3)), ((4, 2), (4, 4))]
 
 ans3 = [((3, 5), (3, 3)), ((4, 2), (2, 2)),
-        ((1, 3), (1, 1)), ((4, 4), (2, 4)),
-        ((5, 5), (3, 5)), ((5, 3), (5, 5)),
-        ((3, 1), (1, 1)), ((4, 0), (4, 2)),
-        ((4, 2), (2, 2)), ((2, 2), (2, 0)),
-        ((2, 4), (2, 2)), ((2, 2), (2, 4))]
+        ((1, 3), (1, 1)), ((3, 3), (1, 3)),
+        ((4, 4), (2, 4)), ((5, 5), (3, 5)),
+        ((4, 0), (4, 2)), ((2, 0), (4, 0)),
+        ((5, 3), (5, 5)), ((4, 2), (4, 4)),
+        ((0, 4), (0, 2)), ((4, 0), (4, 2)),
+        ((5, 1), (5, 3)), ((4, 2), (4, 4))]
+
+ans4 = [((3, 5), (3, 3)), ((4, 2), (2, 2)),
+        ((1, 3), (1, 1)), ((3, 3), (1, 3)),
+        ((5, 5), (3, 5)), ((4, 0), (4, 2)),
+        ((2, 0), (4, 0)), ((3, 1), (1, 1)),
+        ((0, 4), (0, 2)), ((4, 4), (2, 4)),
+        ((5, 3), (5, 5)), ((5, 1), (5, 3)),
+        ((4, 0), (4, 2))]
 
 # specification of boards, gold sequences and game parameters used to test student code
 game1 = [copy.deepcopy(boardA),ans1,100,5,'First Move','MiniMax','o']
 game2 = [copy.deepcopy(boardC),ans2,100,5,'First Move','MiniMax','x']
-game3 = [copy.deepcopy(boardC),ans2,100,5,'First Move','AlphaBeta','x']
-game4 = [copy.deepcopy(boardC),ans3,100,7,'First Move','AlphaBeta','x']
+game3 = [copy.deepcopy(boardC),ans3,100,5,'First Move','AlphaBeta','x']
+game4 = [copy.deepcopy(boardC),ans4,100,7,'First Move','AlphaBeta','x']
 
 games = [game1,game2,game3,game4]
 
-verbose = True # flag to control level of debugging output
+verbose = False # flag to control level of debugging output
 
 # function used to test student implementation for an individual game
 def test(board,max_moves,depth,algoG,algoS,student_xo):
-
     moves_x = []
     moves_o = []
-
+    
     b = kb.KonaneBoard(board)
 
     # instantiates student and grader players based upon students' 'x' or 'o' designation
     if student_xo == 'x':
-        player_x = sc.player(b,'x',depth,algoS)
-        player_o = sc.player(b,'o',depth,algoG)
+        player_x = student.player(b,'x',depth,algoS)
+        player_o = student.player(b,'o',depth,algoG)
     else:
-        player_o = sc.player(b,'o',depth,algoS)
-        player_x = sc.player(b,'x',depth,algoG)
+        player_o = student.player(b,'o',depth,algoS)
+        player_x = student.player(b,'x',depth,algoG)
 
     # displays paramters of specific game used for evaluation
     if verbose:
@@ -102,7 +111,7 @@ def test(board,max_moves,depth,algoG,algoS,student_xo):
                 if verbose:
                     print 'PlayerO moved', move
                     print(b)
-
+                    
     if verbose:
         print('Game Over.')
 
@@ -111,8 +120,7 @@ def test(board,max_moves,depth,algoG,algoS,student_xo):
         return(moves_x)
     else:
         return(moves_o)
-
-
+            
 def main():
     print('Starting Konane tests...\n')
 
@@ -132,10 +140,9 @@ def main():
             print(ans)
         if ans == gold:
             print('  Game %d: PASSED' % i)
-            exit(0)
         else:
             print('  Game %d: FAILED' % i)
-            exit(1)
 
 if __name__ == "__main__":
     main()
+    
