1feb5157715d6dee91e9fad55896a8aff4c1016a
katherinees <katherinesteiner2019@u.northwestern.edu>
2018-03-13 23:01:15 -0500

    added Lab5 files

---
+++ Lab5/student_code.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import math
+import random
+
+class Bayes_Classifier:
+
+    def __init__(self):
+
+
+    def train(self,filename):
+        # code to be completed by students to extract features from training file, and
+        # to train naive bayes classifier.
+
+
+    def classify(self,filename):
+        # code to be completed by student to classifier reviews in file using naive bayes
+        # classifier previously trains.  member function must return a list of predicted
+        # classes with '5' = positive and '1' = negative
+
+
+    
